c915b1eb51d5f8109fea74960faed4927d8c019a
AHAOAHA <ahaoaha_@outlook.com>
2022-12-08 14:44:00 +0800

    update

--- binaries/go.mod
+++ binaries/go.mod
@@ -18,20 +18,31 @@ require (
 )
 
 require (
+	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef // indirect
+	github.com/aymanbagabas/go-osc52 v1.0.3 // indirect
+	github.com/go-openapi/errors v0.20.2 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/huandu/xstrings v1.3.2 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	go.mongodb.org/mongo-driver v1.10.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 	golang.org/x/term v0.2.0 // indirect
 )
 
 require (
+	github.com/go-openapi/strfmt v0.21.3 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
-	github.com/mattn/go-runewidth v0.0.13 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/jedib0t/go-pretty v4.3.0+incompatible
+	github.com/mattn/go-runewidth v0.0.14
+	github.com/muesli/termenv v0.13.0
 	github.com/smartystreets/assertions v1.13.0 // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
 	golang.org/x/crypto v0.3.0 // indirect
---
+++ binaries/internal/todo/theme.go
@@ -0,0 +1,67 @@
+package todo
+
+import (
+	"fmt"
+
+	"github.com/muesli/termenv"
+)
+
+var env = termenv.EnvColorProfile()
+
+// Theme defines a color theme used for printing tables.
+type Theme struct {
+	colorRed     termenv.Color
+	colorYellow  termenv.Color
+	colorGreen   termenv.Color
+	colorBlue    termenv.Color
+	colorGray    termenv.Color
+	colorMagenta termenv.Color
+	colorCyan    termenv.Color
+}
+
+func defaultThemeName() string {
+	if !termenv.HasDarkBackground() {
+		return "light"
+	}
+	return "dark"
+}
+
+func loadTheme(theme string) (Theme, error) {
+	themes := make(map[string]Theme)
+
+	themes["dark"] = Theme{
+		colorRed:     env.Color("#E88388"),
+		colorYellow:  env.Color("#DBAB79"),
+		colorGreen:   env.Color("#A8CC8C"),
+		colorBlue:    env.Color("#71BEF2"),
+		colorGray:    env.Color("#B9BFCA"),
+		colorMagenta: env.Color("#D290E4"),
+		colorCyan:    env.Color("#66C2CD"),
+	}
+
+	themes["light"] = Theme{
+		colorRed:     env.Color("#D70000"),
+		colorYellow:  env.Color("#FFAF00"),
+		colorGreen:   env.Color("#005F00"),
+		colorBlue:    env.Color("#000087"),
+		colorGray:    env.Color("#303030"),
+		colorMagenta: env.Color("#AF00FF"),
+		colorCyan:    env.Color("#0087FF"),
+	}
+
+	themes["ansi"] = Theme{
+		colorRed:     env.Color("9"),
+		colorYellow:  env.Color("11"),
+		colorGreen:   env.Color("10"),
+		colorBlue:    env.Color("12"),
+		colorGray:    env.Color("7"),
+		colorMagenta: env.Color("13"),
+		colorCyan:    env.Color("8"),
+	}
+
+	if _, ok := themes[theme]; !ok {
+		return Theme{}, fmt.Errorf("unknown theme: %s", theme)
+	}
+
+	return themes[theme], nil
+}
