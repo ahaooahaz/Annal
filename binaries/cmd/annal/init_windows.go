4f1263a98cffe8aa09e7093441282868f4f68bc8
AHAOAHA <ahaoaha_@outlook.com>
2023-01-05 11:32:03 +0800

    fit windows

--- binaries/cmd/annal/init_windows.go
+++ binaries/cmd/annal/init_windows.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"path/filepath"
-	"time"
 
 	"github.com/AHAOAHA/Annal/binaries/internal/config"
-	"github.com/AHAOAHA/Annal/binaries/internal/gui"
-	"github.com/AHAOAHA/Annal/binaries/internal/jt"
-	"github.com/AHAOAHA/Annal/binaries/internal/rtmp"
-	"github.com/AHAOAHA/Annal/binaries/internal/storage"
-	"github.com/AHAOAHA/Annal/binaries/internal/todo"
-	"github.com/AHAOAHA/Annal/binaries/internal/version"
-	"github.com/AHAOAHA/encapsutils"
-	"github.com/sirupsen/logrus"
 )
 
 func init() {
@@ -23,7 +12,6 @@ func init() {
 		panic(err.Error())
 	}
 
-	// rootCmd.AddCommand(serve.Cmd) // TODO:
 }
 
 func initEnv() (err error) {
