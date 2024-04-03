e5b23de3904f1a7171505828705f20be52061eba
ahaooahaz <ahaooahaz@outlook.com>
2024-04-03 17:41:33 +0800

    update rime config

--- configs/rime/rime.lua
+++ configs/rime/rime.lua
@@ -1,10 +1,4 @@
--- 两个短横杆代表 lua 的注释行
+-- translators:
 
--- 翻译器：自动转换日期时间
-function date_translator(input, seg)
-   if (input == "date") or (input == "time") then
-      --- Candidate(type, start, end, text, comment)
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), "日期"))
-      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), "时间"))
-   end
-end
+-- 日期时间，可在方案中配置触发关键字。
+date_translator = require("date_translator")
